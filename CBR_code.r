d764584edb30f3922dc25554054945c15de9bafa
erjan-unist <saynavidinov@unist.ac.kr>
2017-02-02 13:56:02 +0900

    Update CBR_code.r

--- CBR_code.r
+++ CBR_code.r
@@ -218,4 +218,24 @@ hist(comparison[,3], xlab="The size of similar cases", main="")
 dev.off()
 
 
-
+seq(0.2,0.8,0.05)
+adjust_result=c()
+for(k in c(4:16)){ 
+  accuracy=sum(comparison[,k]==comparison[,2])/nrow(comparison)
+  type2=sum(comparison[,k]<comparison[,2])/nrow(comparison)
+  type1=sum(comparison[,k]>comparison[,2])/nrow(comparison)
+  one_one=sum((comparison[,k]==1)&(comparison[,2]==1))/sum(comparison[,2]==1)  ### P( pred=1 | true is 1)
+  zero_zero=sum((comparison[,k]==0)&(comparison[,2]==0))/sum(comparison[,2]==0)  ### P( pred=0 | true is 0)
+  adjust_result=rbind(adjust_result,c(accuracy,type1,type2,one_one,zero_zero))
+}
+adjust_result=cbind(adjust_result,seq(0.2,0.8,0.05))
+pdf('rplot.pdf') # Here it is extraction of image to directly to the disk
+plot(adjust_result[,6],adjust_result[,1],lwd=2,type="l", ylim=c(0,1),xlab="Parameter", ylab="Accuracy")
+lines(adjust_result[,6],adjust_result[,2],lwd=2,col=2)
+lines(adjust_result[,6],adjust_result[,3],lwd=2,col=3,lty=2)
+lines(adjust_result[,6],adjust_result[,4],lwd=2,col=4,lty=3)
+lines(adjust_result[,6],adjust_result[,5],lwd=2,col=5,lty=4)
+legend(0.65, 0.8, c("Accuracy", "Type I", "Type II", "P(F=1|A=1)","P(F=0|A=0)"), col = c(1:5),
+       lwd = c(2,2,2,2,2), lty = c(1,1,2,3,4),box.col="white"
+       , bg = "gray90")
+dev.off()
