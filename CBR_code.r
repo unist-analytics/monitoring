12595974dcd0622b7882e97d74c246639f60f767
erjan-unist <saynavidinov@unist.ac.kr>
2017-02-04 13:32:53 +0900

    Update CBR_code.r

--- CBR_code.r
+++ CBR_code.r
@@ -91,24 +91,31 @@ for(j in 1:nrow(testing)){ # by using "for" loop we go through of each row of te
 	similar_cases=training_y
 
 	for(i in 1:length(values)){ # having length of values and by "for" loop we go through each of these values in length
-		if(sum(similar_cases[,check_list[i]]==values[i])>0){   # The size of similar cases should be >0 , because
-		  #
+		if(sum(similar_cases[,check_list[i]]==values[i])>0){   # The size of similar cases should be >0 , becauseto make a prediction 
+	   # at least we need one similarity. even to avoid when there is no similar case we can loosen the criterias by removing least important feature.
+	   #until one similarity is obtained.
 			similar_cases=similar_cases[similar_cases[,check_list[i]]==values[i],]  # similar cases
-		}
+		}       #Below retrieve step is described. similar_cases is equalled similar_cases with looping conditions of check_list and 
+		  #values. here what below code says is: if the values of "i" of value and "i" of similar_cases[check_list] are same, close, 
+		  #similar, if they are equal then similarity is defined. 
+		     
 	}
 
 	prob_delay=sum(similar_cases$y==1)/nrow(similar_cases)  ## calculate probability of delay
 	testing_delay=as.numeric(as.Date(testing[j,"BL_LAST_ETA_DATETIME"]) < as.Date(testing[j,"POD_ATA"])) # delay or not from testing dataset, delay ==> 1
-
-	pred_s_or_f=as.numeric(prob_delay>0.5)  # 0 or 1
+        pred_s_or_f=as.numeric(prob_delay>0.5)  # 0 or 1.  1 means vessel arrives late, 0 means on time. if probabilit is higher than
+	# threshold (0.5) so it means that it is going to be delayed. threshold as 0.5 is reasonable but if you want more conservative decisions
+	# it can be set higher. OR F-measure can be used to make a right choice of threshold.
 	
   # This is for comparison as the thresholds
-	experiments=c()
+	experiments=c()  
 	for(thresholds in seq(0.2,0.8,0.05)){
-		experiments=c(experiments,as.numeric(prob_delay>thresholds))
-	}
+		experiments=c(experiments,as.numeric(prob_delay>thresholds)) #here is experimental part. if prob of testing data collection
+	}     # is higher then delay otherwise no delay.
 	comparison=rbind(comparison,c(pred_s_or_f,testing_delay,nrow(similar_cases),experiments))
 }
+
+#### written above code is all about detection of delay prior to vessel departure. 
 
 comparison[,1:3]
 pred_s_or_f
@@ -120,20 +127,20 @@ s_or_f
 #names(temp)=c("LT","Delay")
 #temp[order(xx),]
 
-comparison=comparison[!is.na(comparison[,1]),]
+comparison=comparison[!is.na(comparison[,1]),]#exclamation sign is logical NOT operator...is.na(comparison) returns TRUE of comparison is missing
 accuracy=sum(comparison[,1]==comparison[,2])/nrow(comparison)
 accuracy
 
 one_one=sum((comparison[,1]==1)&(comparison[,2]==1))/sum(comparison[,2]==1)  ### P( pred=1 | true is 1)
 zero_zero=sum((comparison[,1]==0)&(comparison[,2]==0))/sum(comparison[,2]==0)  ### P( pred=0 | true is 0)
-type2=sum(comparison[,1]<comparison[,2])/nrow(comparison)
-type1=sum(comparison[,1]>comparison[,2])/nrow(comparison)
+type2=sum(comparison[,1]<comparison[,2])/nrow(comparison)#probability that a case is predicted as not delayed when it is in fact delayed.
+type1=sum(comparison[,1]>comparison[,2])/nrow(comparison)#probability that a case is predicted as delayed when it is in fact not delayed.
+## sum of type1 and type2 have to be 1.
 
 #visualization
 comparison2=cbind(comparison[,1:2],(comparison[,1]==comparison[,2])+1)
 plot(comparison2[,1],ylab="Delay(1) or not(0)",col=3,pch=19)
 points(comparison2[,2],col=comparison2[,3],pch=19)
-
 hist(comparison[,3], xlab="The size of similar cases", main="")
 
 #### adjust parameter
@@ -166,7 +173,10 @@ legend(0.65, 0.8, c("Accuracy", "Type I", "Type II", "P(F=1|A=1)","P(F=0|A=0)"),
 
 
 ############################ Feature Selection using similarity score##############################
-library(plyr)
+library(plyr)#install plyr library from R packages to use tools for splitting, applying and combining data
+#for example when you need to break a big problem down into manageable pieces, operate on each piece and 
+#then put all the pieces back together, so this library can used.
+
 
 y=as.numeric(as.Date(training$BL_LAST_ETA_DATETIME) < as.Date(training$POD_ATA))
 
@@ -212,7 +222,38 @@ for (m in 1:length(features)){
 features_ordered=res[order(res[,2],decreasing = T),]
 check_list=features_ordered[1:15,1]
 
+### so the directory of these plots is already set as setwd("E:/opencode/"). setwd("E:/opencode/") is where all of R data files are located
+### so by running code below you can save plots in disk. in our case is "E:/opencode/". 
+### "E:/opencode/" is my working directory....
+
+pdf('rplot.pdf') # by the code below we can directly save our R file plot in disk as pdf file.
+comparison2=cbind(comparison[,1:2],(comparison[,1]==comparison[,2])+1)
+plot(comparison2[,1],ylab="Delay(1) or not(0)",col=3,pch=19)
+points(comparison2[,2],col=comparison2[,3],pch=19)
+hist(comparison[,3], xlab="The size of similar cases", main="")
+dev.off() # dev. off function is to close open plotting
 
 
+seq(0.2,0.8,0.05)# this code is proceeded in the same way as done above. 
+adjust_result=c()
+for(k in c(4:16)){ 
+  accuracy=sum(comparison[,k]==comparison[,2])/nrow(comparison)
+  type2=sum(comparison[,k]<comparison[,2])/nrow(comparison)
+  type1=sum(comparison[,k]>comparison[,2])/nrow(comparison)
+  one_one=sum((comparison[,k]==1)&(comparison[,2]==1))/sum(comparison[,2]==1)  ### P( pred=1 | true is 1)
+  zero_zero=sum((comparison[,k]==0)&(comparison[,2]==0))/sum(comparison[,2]==0)  ### P( pred=0 | true is 0)
+  adjust_result=rbind(adjust_result,c(accuracy,type1,type2,one_one,zero_zero))
+}
+adjust_result=cbind(adjust_result,seq(0.2,0.8,0.05))
+png('rplot.png') # Here it is extraction of image to directly to the disk as an png file.
+plot(adjust_result[,6],adjust_result[,1],lwd=2,type="l", ylim=c(0,1),xlab="Parameter", ylab="Accuracy")
+lines(adjust_result[,6],adjust_result[,2],lwd=2,col=2)
+lines(adjust_result[,6],adjust_result[,3],lwd=2,col=3,lty=2)
+lines(adjust_result[,6],adjust_result[,4],lwd=2,col=4,lty=3)
+lines(adjust_result[,6],adjust_result[,5],lwd=2,col=5,lty=4)
+legend(0.65, 0.8, c("Accuracy", "Type I", "Type II", "P(F=1|A=1)","P(F=0|A=0)"), col = c(1:5),
+       lwd = c(2,2,2,2,2), lty = c(1,1,2,3,4),box.col="white"
+       , bg = "gray90")
+dev.off()
 
 
